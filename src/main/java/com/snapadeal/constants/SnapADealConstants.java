2781efb9ff98af2b46718fd32b27c91a7b826080
Jeevan Nallapparaj <njeevanraj@gmail.com>
2018-06-02 21:19:26 -0700

    Fixing Drop Down Issue in Add Business Page

--- src/main/java/com/snapadeal/constants/SnapADealConstants.java
+++ src/main/java/com/snapadeal/constants/SnapADealConstants.java
@@ -1,4 +1,10 @@
 package com.snapadeal.constants;
 
 public interface SnapADealConstants {
+
+    public static final String CURRENT_USER_KEY = "CURRENT_USER";
+
+    public static final String USER_TYPE_KEY = "USER_TYPE";
+
+    public static final String BUSINESSUSER = "BUSINESSUSER";
 }
--- src/main/java/com/snapadeal/services/SnapADealServices.java
+++ src/main/java/com/snapadeal/services/SnapADealServices.java
@@ -1,23 +1,35 @@
 package com.snapadeal.services;
+import com.snapadeal.constants.SnapADealConstants;
 import com.snapadeal.entity.Location;
 import com.snapadeal.repository.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.geo.*;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.snapadeal.entity.BusinessProfile;
 import com.snapadeal.exceptions.BusinessProfileException;
 import com.snapadeal.repository.BusinessRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpSession;
+
 @Service
-public class SnapADealServices {
+public class SnapADealServices implements SnapADealConstants{
 
     @Autowired
     private BusinessRepository businessRepository;
+
+    @Autowired
+    private HttpSession httpSession;
 
     public BusinessProfile createBusinessAccount(BusinessProfile businessProfile) throws BusinessProfileException {
 
@@ -28,6 +40,8 @@ public class SnapADealServices {
             businessProfile.setPassword(passwordEncoder.encode(businessProfile.getPassword()));
 
             businessRepository.save(businessProfile);
+
+            setSessionContextForBusinessUser(businessProfile.getLogin(), businessProfile.getPassword(), businessProfile);
 
             System.out.println("SnapADealServices:createBusinessAccount() - Business Profile Created for User --> "+businessProfile.getLogin()+" with ID --> "+businessProfile.getId());
         }
@@ -47,4 +61,12 @@ public class SnapADealServices {
         System.out.println (locations );
     }
 
+    public void setSessionContextForBusinessUser(String pLogin, String pPassword, BusinessProfile businessProfile){
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        grantedAuthorities.add (new SimpleGrantedAuthority("BUSINESSUSER"));
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken(pLogin, pPassword, grantedAuthorities));
+        httpSession.setAttribute(CURRENT_USER_KEY, businessProfile);
+        httpSession.setAttribute(USER_TYPE_KEY, BUSINESSUSER);
+    }
 }
