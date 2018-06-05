c69083af0d6b8c698cd9c27994832cd36d2448c2
Jeevan Nallapparaj <njeevanraj@gmail.com>
2018-06-05 00:25:27 -0700

    Adding Service for Reserve Order

---
+++ src/main/java/com/snapadeal/repository/ReservationOrderRepository.java
@@ -0,0 +1,14 @@
+package com.snapadeal.repository;
+
+import com.snapadeal.entity.BusinessProfile;
+import com.snapadeal.entity.ReservationOrder;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.Point;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ReservationOrderRepository extends MongoRepository<ReservationOrder, String>
+{
+    public ReservationOrder findByProfileId(String profileId);
+}
